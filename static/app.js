d8290b4122ba499abbe18f63bf55aef6ef48e441
Cobus Linde <s24162826@gmail.com>
2021-05-08 21:53:12 +0200

    added working remove button to cards

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 "use strict";
-const myLibray = [];
+let myLibrary = [];
 
 function Book(title, author, pages, haveread) {
     this.title = title;
@@ -11,12 +11,17 @@ function Book(title, author, pages, haveread) {
     // }
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "have read" : "not read yet"}`;
-}
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "have read" : "not read yet"}`;
+// }
 
 function addBookToLibrary(book) {
-    myLibray.push(book);
+    myLibrary.push(book);
+}
+
+function removeBookFromLibrary(title) {
+    myLibrary = myLibrary.filter(book => book.title !== title);
+    // console.log(myLibrary);
 }
 
 const addBookButton = document.querySelector(".add_book_button");
@@ -25,18 +30,31 @@ const bookCase = document.querySelector(".bookcase");
 function updateBookCase() {
     bookCase.innerHTML = "";
 
-    for (var book of myLibray) {
+    for (let book of myLibrary) {
         bookCase.appendChild(addBookToShelf(book));
     }
+
+    const closebuttons = bookCase.querySelectorAll(".card .cardclose");
+    for (let closebutton of closebuttons) {
+        closebutton.addEventListener("click", removeBook);
+    }
+}
+
+function removeBook(e) {
+    // console.log(e.target);
+    removeBookFromLibrary(e.currentTarget.id);
+    updateBookCase();
 }
 
 function addBookToShelf(book) {
     const card = document.createElement("div");
+    // const carddiv = document.createElement("div");
     const cardcontainer = document.createElement("div");
     const cardtitle = document.createElement("h4");
     const cardauthor = document.createElement("p");
     const cardpages = document.createElement("p");
     const cardreadbutton = document.createElement("button");
+    const cardclose = document.createElement("div");
 
     card.classList.add("card");
     cardcontainer.classList.add("cardcontainer");
@@ -44,8 +62,19 @@ function addBookToShelf(book) {
     cardauthor.classList.add("cardauthor");
     cardpages.classList.add("cardpages");
     cardreadbutton.classList.add("cardreadbutton");
+    cardclose.classList.add("cardclose");
+    // carddiv.innerHTML = "<span class=\"cardclose\">x</span>"
+    // carddiv.innerHTML += "<img src=\"static/open-book-clip-art_3637477.png\" style=\"width:100%\">"
+    cardclose.innerHTML = "<span>x</span>";
+    cardclose.id = book.title;
+    // cardclose.addEventListener("click", (e) => {
+    //     console.log(e);
+    //     removeBook(e);
+    // });
+    // card.innerHTML = "<span class=\"cardclose\">x</span>";
+    card.appendChild(cardclose);
+    card.innerHTML += "<img src=\"static/open-book-clip-art_3637477.png\" style=\"width:100%\">";
 
-    card.innerHTML = "<img src=\"static/open-book-clip-art_3637477.png\" style=\"width:100%\">"
     cardtitle.textContent = book.title;
     cardauthor.textContent = book.author;
     cardpages.textContent = `Pages: ${book.pages}`;
@@ -55,6 +84,8 @@ function addBookToShelf(book) {
     cardcontainer.appendChild(cardpages);
     cardcontainer.appendChild(cardreadbutton);
     card.appendChild(cardcontainer);
+    // carddiv.appendChild(cardcontainer);
+    // card.appendChild(carddiv);
     return card;
 }
 
@@ -76,12 +107,12 @@ document.addEventListener("DOMContentLoaded", () => {
         read: true
     }];
 
-    for (var book of books) {
+    for (let book of books) {
         // addBookToLibrary(books[entry]);
         // const book = books[entry];
         const tmpbook = new Book(book.title, book.author, book.pages, book.read);
         addBookToLibrary(tmpbook);
     }
     updateBookCase();
-    console.log(myLibray);
+    // console.log(myLibrary);
 })
